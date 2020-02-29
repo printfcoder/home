efe91eef27ffa8e87b9888135912d2e73aa59ce9
shu xian <i@shux.me>
2020-02-29 23:30:30 +0800

    remove width

--- web/app/home-react/src/components/Icon/HIcon.tsx
+++ web/app/home-react/src/components/Icon/HIcon.tsx
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 
 import "../../assets/icon/iconfont.js"
 
-const HIcon = ({name, color, size}: { name: string, color: string, size: number }) => (
+const HIcon = ({name, color, size = 30}: { name: string, color?: string, size?: number }) => (
     <svg aria-hidden="true" style={{
         /*  fontFamily: "iconfont",
           fontSize: "16px",
           fontStyle: "normal",*/
-        width: "1em",
-        height: "1em",
         verticalAlign: "-0.15em",
         fill: "currentColor",
         overflow: "hidden"
